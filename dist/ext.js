46244fc6b1c0cbc3ee679fddd0b83766d271268c
capoch <buemplizer@gmail.com>
2017-06-26 00:23:47 +0700

    first commit

---
+++ dist/ext.js
@@ -0,0 +1,7 @@
+var SweetSweetBasil = (function () {
+    function SweetSweetBasil(name) {
+        console.log("Hello " + name);
+    }
+    return SweetSweetBasil;
+}());
+var basil = new SweetSweetBasil("World");
